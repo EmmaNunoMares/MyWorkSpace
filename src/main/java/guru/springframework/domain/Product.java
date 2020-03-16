ab5d6e54619260ae7f9598dcb410d40c96e6ec4e
Emmanuel <emmanuelnunomares@gmail.com>
2020-03-16 12:29:18 -0600

    JPA product and customer, pre-load and test

--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -1,15 +1,31 @@
 package guru.springframework.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * Created by jt on 11/6/15.
  */
+@Entity
 public class Product implements DomainObject{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
     private String description;
     private BigDecimal price;
     private String imageUrl;
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 
     public Integer getId() {
         return id;
